83783520aabb3a74aa6040e0bbdd3993b4020f6d
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-10 17:24:23 -0600

    Add project files.

---
+++ S5-CoolClock/CoolClockForm.vb
@@ -0,0 +1,12 @@
+﻿'Joshua Makuch
+'RCET 3371
+'Fall 2023
+'Cool Clock
+'https://github.com/JoshuaMakuch/S5-CoolClock
+
+Option Strict On
+Option Explicit On
+
+Public Class CoolClockForm
+
+End Class
