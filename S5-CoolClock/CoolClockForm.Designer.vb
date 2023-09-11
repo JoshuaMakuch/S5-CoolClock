777b0ac48a8b139289b5e48a18cda54b1ffb714a
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-10 21:49:26 -0600

    Done

--- S5-CoolClock/CoolClockForm.Designer.vb
+++ S5-CoolClock/CoolClockForm.Designer.vb
@@ -22,17 +22,100 @@ Partial Class CoolClockForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ClockLabel = New System.Windows.Forms.Label()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.ClockTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Datelabel = New System.Windows.Forms.Label()
+        Me.LightspeedLabel = New System.Windows.Forms.Label()
+        Me.ReachedLabel = New System.Windows.Forms.Label()
+        Me.ClockSpeedLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'ClockLabel
+        '
+        Me.ClockLabel.AutoSize = True
+        Me.ClockLabel.Font = New System.Drawing.Font("3ds", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ClockLabel.Location = New System.Drawing.Point(12, 9)
+        Me.ClockLabel.Name = "ClockLabel"
+        Me.ClockLabel.Size = New System.Drawing.Size(139, 34)
+        Me.ClockLabel.TabIndex = 0
+        Me.ClockLabel.Text = "00:00:00"
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Location = New System.Drawing.Point(359, 178)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(124, 65)
+        Me.QuitButton.TabIndex = 1
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'ClockTimer
+        '
+        Me.ClockTimer.Enabled = True
+        Me.ClockTimer.Interval = 1
+        '
+        'Datelabel
+        '
+        Me.Datelabel.AutoSize = True
+        Me.Datelabel.Font = New System.Drawing.Font("3ds", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Datelabel.Location = New System.Drawing.Point(12, 43)
+        Me.Datelabel.Name = "Datelabel"
+        Me.Datelabel.Size = New System.Drawing.Size(60, 27)
+        Me.Datelabel.TabIndex = 2
+        Me.Datelabel.Text = "Date"
+        '
+        'LightspeedLabel
+        '
+        Me.LightspeedLabel.AutoSize = True
+        Me.LightspeedLabel.Location = New System.Drawing.Point(12, 70)
+        Me.LightspeedLabel.Name = "LightspeedLabel"
+        Me.LightspeedLabel.Size = New System.Drawing.Size(92, 20)
+        Me.LightspeedLabel.TabIndex = 3
+        Me.LightspeedLabel.Text = "LIGHTSPEED"
+        '
+        'ReachedLabel
+        '
+        Me.ReachedLabel.AutoSize = True
+        Me.ReachedLabel.Location = New System.Drawing.Point(12, 110)
+        Me.ReachedLabel.Name = "ReachedLabel"
+        Me.ReachedLabel.Size = New System.Drawing.Size(174, 20)
+        Me.ReachedLabel.TabIndex = 4
+        Me.ReachedLabel.Text = "Where have we reached?"
+        '
+        'ClockSpeedLabel
+        '
+        Me.ClockSpeedLabel.AutoSize = True
+        Me.ClockSpeedLabel.Location = New System.Drawing.Point(12, 90)
+        Me.ClockSpeedLabel.Name = "ClockSpeedLabel"
+        Me.ClockSpeedLabel.Size = New System.Drawing.Size(167, 20)
+        Me.ClockSpeedLabel.TabIndex = 5
+        Me.ClockSpeedLabel.Text = "Cycles of a 5GHz Clock: "
         '
         'CoolClockForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(495, 255)
+        Me.Controls.Add(Me.ClockSpeedLabel)
+        Me.Controls.Add(Me.ReachedLabel)
+        Me.Controls.Add(Me.LightspeedLabel)
+        Me.Controls.Add(Me.Datelabel)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.ClockLabel)
         Me.Name = "CoolClockForm"
         Me.Text = "CoolClockForm"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents ClockLabel As Label
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents ClockTimer As Timer
+    Friend WithEvents Datelabel As Label
+    Friend WithEvents LightspeedLabel As Label
+    Friend WithEvents ReachedLabel As Label
+    Friend WithEvents ClockSpeedLabel As Label
 End Class
--- S5-CoolClock/CoolClockForm.vb
+++ S5-CoolClock/CoolClockForm.vb
@@ -4,9 +4,30 @@
 'Cool Clock
 'https://github.com/JoshuaMakuch/S5-CoolClock
 
-Option Strict On
-Option Explicit On
+Imports System.Configuration
+Imports System.Security.AccessControl
 
 Public Class CoolClockForm
 
+    Dim distanceTraveled As Double
+    Dim clocks As Double
+
+    'When the user presses the quit button
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+        Me.Close()
+    End Sub
+
+    'Every Time the timer clock timer ticks it updates the displayed time to the user's system clock is
+    Private Sub ClockTimer_Tick(sender As Object, e As EventArgs) Handles ClockTimer.Tick
+        ClockLabel.Text = TimeOfDay
+        Datelabel.Text = DateString
+        distanceTraveled = Math.Round((distanceTraveled + 299.792458), 3)
+        clocks = clocks + 5000000
+        LightspeedLabel.Text = "Distance Light Has Traveled (km) : " & CStr(distanceTraveled)
+        ClockSpeedLabel.Text = "How Many Clock Cycles Have Passed Of A 5GHz Clock (Millions): " & CStr(clocks / 1000000)
+        ReachedLabel.Text = "Light Has traveled: " & Math.Round((distanceTraveled / 40000), 1) & " Circumferences Of Earth"
+    End Sub
+
 End Class
+
+
